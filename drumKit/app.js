ea6687eada176b44894b9a14ee63c9277697465d
Yu <maoyu960320@hotmail.com>
2017-10-18 21:18:08 +0800

    fix

--- drumKit/app.js
+++ drumKit/app.js
@@ -1,7 +1,7 @@
 window.addEventListener('keydown',function(e){
     const keyCode = e.keyCode;
     const selectedKey = document.querySelector('.key-'+keyCode);
-    const selectedAudio = document.querySelector('audio[data-key="'+keyCode+'"]')
+    const selectedAudio = document.querySelector('audio[data-key="'+keyCode+'"]');
     selectedAudio.play();
     console.log(selectedAudio);
     selectedKey.classList.add('pressed');
