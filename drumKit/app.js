d33c4e02d9c38504b8db70284d5a562841b2eb1f
Yu <maoyu960320@hotmail.com>
2017-10-18 21:17:38 +0800

    替换classList的toggle方法为add和remove

--- drumKit/app.js
+++ drumKit/app.js
@@ -4,11 +4,11 @@ window.addEventListener('keydown',function(e){
     const selectedAudio = document.querySelector('audio[data-key="'+keyCode+'"]')
     selectedAudio.play();
     console.log(selectedAudio);
-    selectedKey.classList.toggle('pressed');
-})
+    selectedKey.classList.add('pressed');
+});
 
 window.addEventListener('keyup',function(e){
     const keyCode = e.keyCode;
     const selectedKey = document.querySelector('.key-'+keyCode);
-    selectedKey.classList.toggle('pressed');
-})
+    selectedKey.classList.remove('pressed');
+});
